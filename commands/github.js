09300268395243f5126d9e8539ff07f5e6d361bb
qweri0p <71005650+qweri0p@users.noreply.github.com>
2021-11-10 17:08:41 +0100

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,17 +1,14 @@
 const simpleGit = require('simple-git');
-const git = simpleGit();
 
 function update(message) {
     if (message.author.id == '354943770464354306') {
-        git.pull();
+        simpleGit.pull();
         message.channel.send('Updated!');
     } else {
         message.channel.send('Only qweriop#0416 can update this bot.');
     }
 }
-
 function gh(message) {
   message.channel.send("https://github.com/qweri0p/MexicoBot");
 }
-
 module.exports = { update, gh };
